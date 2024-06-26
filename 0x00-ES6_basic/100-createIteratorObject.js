12cd6f01a23350c720ca5016da94b9f312e87487
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-26 20:46:34 +0100

    fixed eslint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,9 @@
-import createReportObject from './12-createReportObject';
-const report = createReportObject();
-
 export default function createIteratorObject(report) {
-    const newlist = [];
-    for (const department of Object.values(report.allEmployees)) {
-        for (const employees of department) {
-            newlist.push(employees)
-        }
-        
+  const newlist = [];
+  for (const department of Object.values(report.allEmployees)) {
+    for (const employees of department) {
+      newlist.push(employees);
     }
-    return newlist;
+  }
+  return newlist;
 }
