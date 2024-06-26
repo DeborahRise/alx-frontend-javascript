ec963da75168b1b3984d4738d374dff5524c8e13
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-26 19:40:12 +0100

    12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,16 +1,11 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
-
-// const employeesList = createEmployeesObject(department, employees);
-
 export default function createReportObject(employeesList) {
-    return {
-        allEmployees: {
-            ...employeesList,
-        },
-        
-        getNumberOfDepartments(employeesList) {
-            return Object.keys(this.allEmployees).length;
-        }
-    }
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
 
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
 }
