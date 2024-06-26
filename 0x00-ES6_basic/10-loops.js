f58e9cf92c526c6719b5c1d4a871dee3f6bb70f6
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-26 16:47:51 +0100

    eslint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let newarray = [];
-    for (const value of array) {
-      newarray.push(appendString + value);
-    }
-    return newarray;
+  const newarray = [];
+  for (const value of array) {
+    newarray.push(appendString + value);
   }
+  return newarray;
+}
