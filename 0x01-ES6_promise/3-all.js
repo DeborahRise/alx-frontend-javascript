622ac5cc4bd198d38589bc7883115b47b9e30de0
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-27 05:12:21 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([uploadPhoto, createUser]) => {
-      console.log(uploadPhoto.body, createUser.firstName, createUser.lastName);
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
     })
     .catch(() => {
       console.log('Signup system offline');
