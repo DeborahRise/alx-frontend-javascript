487bec3d2727c35fc94a04e91d177d30241c672d
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-27 20:05:32 +0100

    i removed the initialized variable. no need apparently

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,5 @@
 export default class ClassRoom {
-    _maxStudentsSize;
-
-    constructor (maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
