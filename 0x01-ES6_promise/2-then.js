2737b6fc2971fb57d7af70990b9488129b1d9472
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-27 03:47:00 +0100

    eslint fixed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-    return promise
-      .then(() => {
-        return {
-            status: 200,
-            body: 'Success',
-        };
-      })
-      .catch(() => {
-        return Error;
-      })
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => Error)
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
