bf957a81652c40c654e9944e44de0470eb8c82a6
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-27 05:34:34 +0100

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import  uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    const promise1 = signUpUser(firstName, lastName);
+    const promise2 = uploadPhoto(fileName);
+
+    return Promise.allSettled([promise1, promise2]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.message,
+    }))
+  );
+}
