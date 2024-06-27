4c4755c84fc62b8f151e53b1bd990d6f2c632471
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-27 03:02:21 +0100

    eslint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,25 +1,25 @@
 export default function getFullResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const success = true;
-            if (success) {
-                resolve({
-                    status: 200,
-                    body: 'Success'
-                });
-            } else {
-                reject({
-                    Error: 'The fake API is not working currently'
-                })
-            }
-        }, 2000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject({
+          Error: 'The fake API is not working currently',
+        });
+      }
+    }, 2000);
+  });
 }
 
 getFullResponseFromAPI()
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
