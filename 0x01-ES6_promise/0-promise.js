de17400e0dbf8a86d3c1ab46565b5b964c120cc5
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-27 02:38:54 +0100

    eslint expected promise rejection to be Error as in keyword

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,19 +1,19 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const success = true;
-            if (success) {
-                resolve('Successful');
-            } else {
-                reject('Unsucessful');
-            }
-        }, 2000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+      if (success) {
+        resolve('Successful');
+      } else {
+        reject(Error);
+      }
+    }, 2000);
+  });
 }
 getResponseFromAPI()
-    .then((response) => {
-        console.log(response)
-})
-    .catch((error) => {
-        console.log(error);
-    })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
