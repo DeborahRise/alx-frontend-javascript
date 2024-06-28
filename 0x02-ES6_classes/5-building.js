220bb81f9aed7c84f09854e1f1fcfa87bb13cea5
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-28 05:10:35 +0100

    used .prototype .function to check

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,7 +2,8 @@ export default class Building {
   constructor(sqft) {
     this._sqft = Building.isNumber(sqft);
 
-    if (this.evacuationWarningMessage === undefined) {
+    if (new.target !== Building
+        && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
