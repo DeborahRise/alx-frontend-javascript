a9f0d1bf907e8e289d089869db561b67e4708739
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-28 04:31:05 +0100

    eslint doner

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,27 +1,27 @@
-import Building from "./5-building";
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft)
-        this._floors = SkyHighBuilding.isNumber(floors);
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = SkyHighBuilding.isNumber(floors);
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = SkyHighBuilding.isNumber(floors);
+  }
+
+  static isNumber(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    return value;
+  }
 
-    get floors() {
-        return this._floors;
-      }
-
-    set floors(floors) {
-        this._floors = SkyHighBuilding.isNumber(floors);
-    }
-
-    static isNumber(value) {
-        if (typeof value !== 'number') {
-          throw new TypeError('Length must be a number');
-        }
-        return value;
-      }
-
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors`
-      }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
