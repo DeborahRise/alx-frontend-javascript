48a5e31053a418f5f0add5fbf046e3b43f498089
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-28 03:35:38 +0100

    pending eslint error, but output is corrected

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,40 +1,39 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-        this._amount = Pricing.isNumber(amount);
-        this._currency = currency
+  constructor(amount, currency) {
+    this._amount = Pricing.isNumber(amount);
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = Pricing.isNumber(amount);
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  static isNumber(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    return value;
+  }
 
-    get amount() {
-        return this._amount;
-      }
-    
-      get currency() {
-        return this._currency;
-      }
-    
-      set amount(amount) {
-        this._amount = Pricing.isNumber(amount);
-      }
-    
-      set currency(currency) {
-        currency = new Currency();
-        this._currency = currency;
-      }
-    
-      static isNumber(value) {
-        if (typeof value !== 'number') {
-          throw new TypeError('Length must be a number');
-        }
-        return value;
-      }
-      displayFullPrice() {
+  displayFullPrice() {
+    return (`${this._amount} ${this._currency.displayFullCurrency()}`);
+  }
 
-        return (`${this._amount} ${this._currency.displayFullCurrency()}`)
-      }
-      static convertPrice(amount, conversionRate) {
-        
-        return (`${amount} * ${conversionRate}`)
-      }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
