65cb90da3785dc63f32166ff08701d763bc05867
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-28 02:03:07 +0100

    eslint fixed

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,30 +1,33 @@
 export default class Currency {
-    constructor(code, name) {
-        this._code = Currency.isString(code);
-        this._name = Currency.isString(name);
-    }
+  constructor(code, name) {
+    this._code = Currency.isString(code);
+    this._name = Currency.isString(name);
+  }
 
-    get code() {
-        return this._code;
-    }
-    get name() {
-        return this._name;
-    }
-    set code(code) {
-        this._code = Currency.isString(code);
-    }
-    set name(name) {
-        this._name = Currency.isString(name);
-    }
+  get code() {
+    return this._code;
+  }
 
-    static isString(value) {
-        if (typeof value !== 'string') {
-          throw new TypeError('Value must be a string');
-        }
-        return value;
-      }
+  get name() {
+    return this._name;
+  }
 
-      displayFullCurrency() {
-        console.log(`${this._name} (${this._code})`);
-      }
+  set code(code) {
+    this._code = Currency.isString(code);
+  }
+
+  set name(name) {
+    this._name = Currency.isString(name);
+  }
+
+  static isString(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Value must be a string');
+    }
+    return value;
+  }
+
+  displayFullCurrency() {
+    console.log(`${this._name} (${this._code})`);
+  }
 }
