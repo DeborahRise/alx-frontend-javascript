be73729c41d35c4b9503bf100147b6bc4c45b341
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-01 15:26:03 +0100

    return wasnt added .. HMMMM

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then(([uploadPhotoResult, createUserResult]) => {
-      const { body } = uploadPhotoResult;
-      const { firstName, lastName } = createUserResult;
-      console.log(`${body} ${firstName} ${lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  Promise.all([uploadPhoto(), createUser()])
+  .then(([photoResult, userResult]) => {
+    console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+  })
+  .catch(() => {
+    console.log('Signup system offline');
+  });
 }
