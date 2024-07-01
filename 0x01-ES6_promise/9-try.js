ac4e0205384bb9e8100348a3f5ce52038bc319f7
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-01 19:03:22 +0100

    toString() was the issue. i USED .message before

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
