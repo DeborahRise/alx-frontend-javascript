f90abae47770d8d41d6fb3a59716e33aa362e35f
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-01 17:29:21 +0100

    correct output3

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,9 +5,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
 
-  return Promise.allSettled([promise1, promise2]).then((results) => results.map((result) => ({
+  return Promise.allSettled([promise1, promise2]).then((results) => results.map((result) => (
+    [{
     status: result.status,
     value: result.status === 'fulfilled' ? result.value : result.reason.message,
-  })
+  }],
+);
 ));
 }
