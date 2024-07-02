8c23303827b31365742045d3b96c4d33e816441e
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-02 17:48:17 +0100

    fixed eslint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default function getListStudents() {
-    return [
-        {
-        id: 1,
-        firstName: 'Guillaume',
-        location: 'San Francisco'
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
     },
     {
-        id: 2,
-        firstName: 'James',
-        location: 'Columbia'
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
     },
     {
-        id: 3,
-        firstName: 'Serena',
-        location: 'San Francisco'
-    }
-]
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 }
