f596a9e2d1caecdee7a2acc680c9c377b8133dca
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-02 23:22:24 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-    const buffer = new ArrayBuffer(length);
-    
-    if (position >= 0 && position < length) {
-        const dataview = new DataView(buffer);
-        dataview.setInt8(position, value);
-        return dataview;
-    } else {
-        throw new Error('Position outside range')
-    }
-    
+  const buffer = new ArrayBuffer(length);
+
+  if (position >= 0 && position < length) {
+    const dataview = new DataView(buffer);
+    dataview.setInt8(position, value);
+    return dataview;
+  }
+  throw new Error('Position outside range');
 }
