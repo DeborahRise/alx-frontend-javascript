3aadb2a8f2adf53da97e9898abe5dde3797e24c6
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-03 05:49:43 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
   const result = [];
